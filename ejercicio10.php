93cb2e2d53bd721e530de373dd5f908c7a666575
CodeBot82 <134826374+CodeBot82@users.noreply.github.com>
2024-08-09 09:06:24 -0400

    Add files via upload

---
+++ ejercicio10.php
@@ -0,0 +1,12 @@
+<?php
+
+// Crea un programa que imprima la fecha y hora actual 
+
+// Codebots 
+
+$fecha = date("d-m-Y");
+$hora = date("H:i:s");
+
+echo "Fecha: ". $fecha . "<br>";
+echo "Hora: " . $hora;
+?>
